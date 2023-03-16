d2f8f57f198b0eedbce0adba7434eb8209facaa2
sosring <hiperlogi@gmail.com>
2023-03-16 20:00:03 +0530

    added social links

--- components/Socials.vue
+++ components/Socials.vue
@@ -1,16 +1,21 @@
 <template>
   
-  <div class="social-wrapper hidden
-   md:grid gap-4 fixed bottom-8 left-8">
-    <i v-for="social in socials"
+  <div class="hidden md:grid 
+   gap-4 fixed bottom-8 left-8">
+    <a v-for="social in socials"
      class="text-3xl text-secoundary"
-     :class="`fab fa-${social.icon}`"></i>
+     :class="`fab fa-${social.icon}`"
+     target="blank"
+     :href="social.link"></a>
   </div>
 </template>
 
 <script setup>
+  import { useDetailStore } from '../stores/useDetails.js'
+  const useDetails = useDetailStore()
+
   const socials = [
-    { icon: 'linkedin', link: '' },
-    { icon: 'github', link: '' }
+    { icon: 'linkedin', link: useDetails.linkedIn },
+    { icon: 'github', link: useDetails.github }
   ]
 </script>
