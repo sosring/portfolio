93165347f7877b4406f1f402f2007038612e07f1
sosring <hiperlogi@gmail.com>
2023-03-24 21:54:52 +0530

    email functionality needs to be added

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,13 +1,13 @@
 <template>
-  <section class="h-screen container 
-   mx-auto px-4 py-12 font-montserrat
+  <section class="container mx-auto px-4 
+   pt-12 md:pt-16 font-montserrat
    flex items-center justify-center">
 
     <article class="text-center">
 
       <h2 class="heading stroke 
        text-secoundary font-thin
-       mb-4 sm:mb-6 font-thin "
+       mb-4 sm:mb-6 "
        id="contact">
         Get in touch
       </h2>
